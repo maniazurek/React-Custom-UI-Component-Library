e37aab01717e54bb3af7fd8716c9a71fe55b5ef8
Marianna Zurek <zurekmarianna@gmail.com>
2023-01-25 00:09:17 +0100

    added accordion component

--- src/styled-components-website/SectionDescription.js
+++ src/styled-components-website/SectionDescription.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const SectionDescription = styled.p`
   font-size: 13px;
-  margin: 0px;
+  margin-top: 0px;
+  margin-bottom: 30px;
 `;
 
 export default SectionDescription;
