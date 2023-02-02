5f479e3dd179bc3c8cbed7991f7318aba9f0a897
Marianna Zurek <zurekmarianna@gmail.com>
2023-02-02 22:31:52 +0100

    worked on transfer list component

--- src/components-UI/TransferList/TransferListUI.js
+++ src/components-UI/TransferList/TransferListUI.js
@@ -1,12 +1,38 @@
-import React from "react";
-
-import randomData from "../../utils/checkboxData";
+import React, { useState } from "react";
+import transferListData from "../../utils/transferListData";
 
 const TransferListUI = () => {
+  const [selected, setSelected] = useState(null);
+  const [defaultArray, setDefaultArray] = useState(transferListData);
+  const [selectedArray, setSelectedArray] = useState([]);
+
+  const onSelect = (index) => {
+    setSelected(index);
+  };
+
+  const onAddToSelectedArray = (selected) => {
+    setDefaultArray(
+      defaultArray.filter((item) => defaultArray.indexOf(item) !== selected)
+    );
+    setSelectedArray([...selectedArray, defaultArray[selected]]);
+  };
+
+  const onAddToDefaultArray = (selected) => {
+    setSelectedArray(
+      selectedArray.filter((item) => selectedArray.indexOf(item) !== selected)
+    );
+    setDefaultArray([...defaultArray, selectedArray[selected]]);
+  };
+
   return (
     <>
-      {randomData.map((data) => (
-        <div>{data.name}</div>
+      {defaultArray.map((data, index) => (
+        <div onClick={() => onSelect(index)}>{data.name}</div>
+      ))}
+      <div onClick={() => onAddToSelectedArray(selected)}>add</div>
+      <div onClick={() => onAddToDefaultArray(selected)}>remove</div>
+      {selectedArray.map((data, index) => (
+        <div onClick={() => onSelect(index)}>{data.name}</div>
       ))}
     </>
   );
